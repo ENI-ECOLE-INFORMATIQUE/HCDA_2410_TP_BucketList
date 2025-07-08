988aa5ba04ef2514148f830442d8be12d09156b4
dsanchez <dsanchez@campus-eni.fr>
2025-07-08 16:15:47 +0200

    Création des fixtures maj de la css création de la liste des wishes et du détail

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,16 +10,24 @@ use Symfony\Component\Routing\Attribute\Route;
 final class WishController extends AbstractController
 {
     #[Route('/', name: 'list')]
-    public function list(): Response
+    public function list(WishRepository $whishRepository): Response
     {
+        $wishes = $whishRepository->findBy(['isPublished' => true], ['dateCreated' => 'DESC']);
         return $this->render('wish/list.html.twig', [
+            "wishes" => $wishes
         ]);
     }
 
     #[Route('/wishes/{id}', name: 'detail', methods: ['GET'],requirements: ['id'=>'\d+'])]
-    public function detail(int $id): Response
+    public function detail(int $id,WishRepository $wishRepository): Response
     {
-        return $this->render('wish/detail.html.twig', [
+        //récupère le wish en fonction de l'id présent dans l'URL
+        $wish = $wishRepository->find($id);
+        //S'il n'existe pas en bdd, on déclenche une erreur 404
+        if(!$wish){
+            throw $this->createNotFoundException('Wish not found, Sorry !');
+        }
+        return $this->render('wish/detail.html.twig', ["wish" => $wish
         ]);
     }
 }
